5b01779ec72199134eab8d16c6a70de9d1333277
李亚清 <liyaqing@qutoutiao.net>
2019-07-13 13:51:54 +0800

    Revert "dd"
    This reverts commit 7d3b3743

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("qing==", "onCreate: "+ Build.MODEL);
+//        NotificationUtils.openNotificationSettings(getApplicationContext());
         RadioButton radioButton =findViewById(R.id.rb);
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
             radioButton.setButtonDrawable(new ColorDrawable(Color.TRANSPARENT));
